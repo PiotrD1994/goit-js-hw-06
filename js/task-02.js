6d317ac373b262fde048c7c5f75f181a85e4a887
Piotr Dubiel <piotrdubiel30@gmail.com>
2023-07-27 21:55:41 +0200

    corrections

--- js/task-02.js
+++ js/task-02.js
@@ -7,13 +7,13 @@ const ingredients = [
   "Condiments",
 ];
 const list = document.querySelector("#ingredients")
-const addItem = []
+const liNodes= []
 
 ingredients.forEach((item) => {
 const listItem = document.createElement("li")
 listItem.textContent = item
 listItem.classList.add("item")
-addItem.push(listItem)
+liNodes.push(listItem)
 })
 
-list.append(...itemsToAdd)
+list.append(...liNodes)
